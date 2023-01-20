f500bec46cb74aed65f9191158e9878f59a52dd6
Robby Lewis <Robbysim37@gmail.com>
2023-01-19 19:12:36 -0600

    finished general quiz list layout

---
+++ frontend/qizurd-frontend/src/components/quiz-components/QuizCard.js
@@ -0,0 +1,20 @@
+import React from "react"
+import styled from "styled-components"
+
+const QuizCard = styled.div`
+display:flex;
+flex-direction:column;
+justify-content: space-around;
+align-items:center;
+text-align:center;
+background-color: orange;
+width: 80vw;
+height: 3rem;
+padding: 1rem;
+margin: 1rem;
+border-radius: 10px;
+box-shadow: 5px 5px 1px black;
+cursor:pointer;
+`
+
+export default QuizCard
--- frontend/qizurd-frontend/src/components/quiz-components/QuizList.js
+++ frontend/qizurd-frontend/src/components/quiz-components/QuizList.js
@@ -2,31 +2,12 @@ import React from "react"
 import styled from "styled-components"
 import { connect } from "react-redux"
 import { cardClicked } from "../../state/action-builder"
+import QuizCard from "./QuizCard"
 
 const QuizContainer = styled.div`
 display: flex;
 flex-direction: column;
 align-items: center;
-`
-
-const QuizCard = styled.div`
-display:flex;
-flex-direction:column;
-justify-content: space-around;
-align-items:center;
-text-align:center;
-background-color: orange;
-width: 80vw;
-height: 3rem;
-padding: 1rem;
-margin: 1rem;
-border-radius: 10px;
-box-shadow: 5px 5px 1px black;
-${props => props.pressed ? 'box-shadow: inset 5px 5px 1px black;' : 'box-shadow: 5px 5px 1px black;'}
-`
-
-const CardText = styled.p`
-text-size: 1rem;
 `
 
 const QuizList = (props) => {
@@ -39,7 +20,7 @@ const QuizList = (props) => {
         <QuizContainer>
             {props.quizzes.map( el => {
                 return <QuizCard id={el.id} key={el.id} pressed={el.pressed} onClick={quizCardClickHandler}>
-                    <CardText>{el.quizTitle}</CardText>
+                    {el.quizTitle}
                 </QuizCard>
             })}
         </QuizContainer>

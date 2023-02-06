3159c7ae9b91bb0f72222af8cfc98346cf4f4658
Robert Lewis <robsim37@gmail.com>
2023-02-06 16:04:11 -0500

    rem security files

--- backend/utils/dbUtils.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    url: 'mongodb+srv://qizurd-c0.d5jin3p.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority',
-    cert: "backend/permissions/X509-cert-5519782101891610673.pem"
-};

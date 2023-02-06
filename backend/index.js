02ed0880399cefd3972847813acbf921498a78f2
Robert Lewis <robsim37@gmail.com>
2023-02-06 13:16:38 -0500

    added time utility

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ server.use(bodyParser.urlencoded({ extended: true }))
 server.use(cors());
 
 const data = require("./layers/data/data");
+const timeUtils = require("./utils/timeUtils");
 
 const quizCtrl = require("./layers/controllers/quizController");
 const userCtrl = require("./layers/controllers/usersController");
@@ -35,4 +36,5 @@ server.get("/students/:userId",studentCtrl.allStudents);
 
 server.listen(PORT, ()=>{console.log(`Server up and listening on port ${PORT}`)});
 
+//setInterval(()=>{data.saveDataToDB()},timeUtils.convertToMilliseconds(1,"day"));
 //process.on("SIGINT", ()=>{data.saveDataToDB(); process.exit()});

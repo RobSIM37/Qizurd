2dba370edbaeb208608747fa3adda8bdb912926a
Robby Lewis <Robbysim37@gmail.com>
2023-02-09 14:35:19 -0500

    mongodb cert

---
+++ backend/utils/dbUtils.js
@@ -0,0 +1,4 @@
+module.exports = {
+    url:"mongodb+srv://qizurd-c0.d5jin3p.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority",
+    cert:"backend/permissions/X509-cert-461764255569896597.pem"
+}

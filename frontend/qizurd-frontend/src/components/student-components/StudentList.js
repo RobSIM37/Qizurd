78bf0284211a6dfa385bc2f830f9d6863b50e794
Robby Lewis <Robbysim37@gmail.com>
2023-02-16 19:55:04 -0500

    trying to fix stylings

--- frontend/qizurd-frontend/src/components/student-components/StudentList.js
+++ frontend/qizurd-frontend/src/components/student-components/StudentList.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { connect } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { Card,Container,Typography,Box } from "@mui/material"
+import { Card,Container,Typography,Box,IconButton } from "@mui/material"
+import DeleteIcon from '@mui/icons-material/Delete';
 import { fillStudentForm,deleteStudent } from "../../state/action-builder"
 
 //feed this component a list of students.
@@ -34,8 +35,12 @@ const StudentList = (props) => {
             {props.clickHandlerid === "editStudent" && <Typography fontSize="2rem">Edit a Student</Typography>}
             {props.user.students.map(el => {return (
                 <Box mt={2} mb={2}>
-                    <Card id={el.id} onClick={clickHandlerControl}>
-                        <Typography sx={{pointerEvents:"none"}} mt={1} mb={1} fontSize="1.5rem">{el.firstName + " " + el.lastName}</Typography>
+                    <Card sx={{display:"flex",flexDirection:"row"}} id={el.id} onClick={clickHandlerControl}>
+                        <Typography sx={{pointerEvents:"none",width:"90%",position:"static"}} mt={1} mb={1} fontSize="1.5rem">{el.firstName + " " + el.lastName}</Typography>
+                        {props.clickHandlerid === "editStudent" && 
+                            <IconButton>
+                                <DeleteIcon/>
+                            </IconButton>}
                     </Card>
                 </Box>)})}
         </Container>

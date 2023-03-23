f2c8d4f65d7a296d90da66aa43f85bdd5b715671
Robert Lewis <robsim37@gmail.com>
2023-03-23 17:39:02 -0400

    cleaned up whitespace

--- backend/index.js
+++ backend/index.js
@@ -21,21 +21,13 @@ server.post("/quizzes", quizCtrl.addOrUpdateQuiz);
 server.get("/quizzes/:userId/:quizId", quizCtrl.getQuiz);
 server.get("/quizzes/:userId", quizCtrl.getAllQuizzes);
 server.delete("/quizzes/:userId/:quizId", quizCtrl.deleteQuiz);
-server.get(
-  "/quizzes/students/ranked/:userId/:quizId",
-  quizCtrl.getRankedListOfStudents
-);
+server.get("/quizzes/students/ranked/:userId/:quizId",quizCtrl.getRankedListOfStudents);
 server.get("/questions/:userId/:quizId/:questionId", quizCtrl.getQuestion);
-server.get(
-  "/questions/random/:userId/:quizId/:studentId",
-  quizCtrl.getRandomQuestionForStudent
-);
+server.get("/questions/random/:userId/:quizId/:studentId",quizCtrl.getRandomQuestionForStudent);
 
 server.post("/students", studentCtrl.addOrUpdateStudent);
 server.delete("/students/:userId/:studentId", studentCtrl.deleteStudent);
 server.get("/students/:userId", studentCtrl.allStudents);
 server.post("/students/answer", studentCtrl.logStudentAnswer);
 
-server.listen(PORT, () => {
-  console.log(`Server up and listening on port ${PORT}`);
-});
+server.listen(PORT, () => {console.log(`Server up and listening on port ${PORT}`);});
